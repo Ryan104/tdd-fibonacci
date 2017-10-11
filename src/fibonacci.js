e81b7a3f9dfe38d0faee252a2434516fa73d07ae
Ryan <ryan.d.elliott@gmail.com>
2017-10-11 15:34:35 -0600

    passing all tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -5,4 +5,16 @@
      * @param {Number} n The index of the sequence to calculate; zero-based.
      * @returns {Number} the nth element of the Fibonacci sequence.
      */
-    // TODO: return definition of `fibonacci` function here
+
+function fibonacci(index){
+	if (index < 0 || typeof index !== 'number'){
+		return 0;
+	}
+	if (index === 0 || index === 1){
+		return 1;
+	} else {
+		return fibonacci(index - 1) + fibonacci(index - 2);
+	}
+}
+
+module.exports = fibonacci;
